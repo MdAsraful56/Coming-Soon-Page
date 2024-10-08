885451be79001c117abdde959c024b2ce6f0c008
ASHRAFUl45756 <ashrafulislamasrf756@gmail.com>
2024-10-09 01:46:19 +0600

    done

---
+++ script.js
@@ -0,0 +1,16 @@
+let countDownDate = new Date("oct 11, 2024 00:00:00").getTime();
+
+let x = setInterval(function(){
+        let now =  new Date().getTime();
+        let distance = countDownDate - now;
+
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor(distance % (1000 * 60 * 60* 20) / (1000 * 60 * 60));
+        let minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+        let seconds = Math.floor(distance % (1000 * 60) / 1000);
+
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+}, 1000);
